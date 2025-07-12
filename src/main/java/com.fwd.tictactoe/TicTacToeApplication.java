377bb659c43ed9c8a9245b3806b4758c3fc1e736
rinda <rinda.putri@indivaragroup.com>
2025-07-12 11:46:30 +0700

    commit test

---
+++ src/main/java/com.fwd.tictactoe/TicTacToeApplication.java
@@ -0,0 +1,12 @@
+package com.indivaragroup.woi.mobile.awsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AwsGatewayApplication.class, args);
+    }
+
+}
